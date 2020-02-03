62495e19e096c5d7c86b325514e78aed485d8f6a
lljb01 <59439485+lljb01@users.noreply.github.com>
2020-02-03 10:45:00 +0000

    Add files via upload

---
+++ Write_a_file_to_text/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_a_file_to_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string line = Console.ReadLine();
+
+            // Set a variable to the My Documents path.
+            string mydocpath =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            //Write the string array to a new file named "writeLine.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath, "Data_Export.txt")))
+            {
+                //foreach (string line in lines)
+                outputFile.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Write_a_file_to_text/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Write_a_file_to_text")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LTE Group")]
+[assembly: AssemblyProduct("Write_a_file_to_text")]
+[assembly: AssemblyCopyright("Copyright © LTE Group 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("691b4489-9577-4dbd-b0e4-417ebd6064d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
